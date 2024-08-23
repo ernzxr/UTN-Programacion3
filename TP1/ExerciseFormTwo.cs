f24636698372de037b7f38208253369c4ea27cb1
VickyLuques <luquesvictoria559@gmail.com>
2024-08-23 16:39:48 -0300

    Validación para nombres repetidos, ej 2

--- TP1/ExerciseFormTwo.cs
+++ TP1/ExerciseFormTwo.cs
@@ -27,18 +27,39 @@ namespace TP1
         private void btnAgregar_Click(object sender, EventArgs e)
         {
             //Si lo que hay en textNombre y txtApellido esta vacio
-            if (txtNombre.Text != "" && txtApellido.Text != "")
+            if (txtNombre.Text.Trim() != "" && txtApellido.Text.Trim() != "")
             {
-                // Agrego el nombre completo al ListBox
-                lbLista.Items.Add(txtNombre.Text + " " + txtApellido.Text);
+               String nCompleto = txtNombre.Text + " " + txtApellido.Text;//concateno el nombre y el apellido antes de agregarlo a la lista
 
-                // luego limpio los TextBox
-                txtNombre.Clear();
-                txtApellido.Clear();
+                if (lbLista.Items.Count <= 0) //verifico si el listbox esta vacio
+                {
+                    lbLista.Items.Add(nCompleto);//si esta vacio agrego el nombre completo
+                    // luego limpio los TextBox
+                    txtNombre.Clear();
+                    txtApellido.Clear();
+                }
+                else
+                {
+                    int index = lbLista.FindString(nCompleto);//si la lista contiene elementos busco dentro del listbox si el nombre completo existe
+                    if (index < 0)
+                    {
+                        lbLista.Items.Add(nCompleto); //si no existe lo agrego
+                        txtNombre.Clear();
+                        txtApellido.Clear();
+                    }
+                    else
+                    {
+                        //Si esta repetido, muestro un cartel aclaratorio
+                        MessageBox.Show("El nombre y apellido " + nCompleto + " ya existe en la lista. Ingrese otro nombre y apellido.");
+                        txtNombre.Clear();
+                        txtApellido.Clear();
+                    }
+                }
+                                                                 
             }
             else
             {
-                // muestro mensaje aclaratorio en caso de no complir con la condicion 
+                // muestro mensaje aclaratorio en caso de no cumplir con la condicion 
                 MessageBox.Show("Debe completar ambos campos.");
             }
         }
