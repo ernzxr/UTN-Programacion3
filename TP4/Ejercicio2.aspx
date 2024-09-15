1990126ce71367af2d8b4d697362766772b33721
Ernesto José Riveiro <ernesto.ernzxr@gmail.com>
2024-09-15 17:15:12 -0300

    Se ajusta el codigo de filtro y se agrega filtro por categoria

--- TP4/Ejercicio2.aspx
+++ TP4/Ejercicio2.aspx
@@ -11,11 +11,47 @@
             width: 60%;
             margin-left: 1%;
         }
-        .auto-style2 {
-            height: 83px;
-        }
         .auto-style3 {
             margin-left: 20px;
+        }
+        .auto-style5 {
+            height: 83px;
+            }
+        .auto-style9 {
+            width: 274px;
+        }
+        .auto-style10 {
+            height: 83px;
+            width: 274px;
+        }
+        .auto-style11 {
+            width: 274px;
+            height: 26px;
+        }
+        .auto-style13 {
+            height: 26px;
+        }
+        .auto-style14 {
+            width: 106px;
+            height: 26px;
+        }
+        .auto-style16 {
+            height: 83px;
+            width: 106px;
+        }
+        .auto-style17 {
+            width: 106px;
+        }
+        .auto-style18 {
+            width: 53px;
+            height: 26px;
+        }
+        .auto-style19 {
+            width: 53px;
+        }
+        .auto-style20 {
+            height: 83px;
+            width: 53px;
         }
     </style>
 </head>
@@ -24,30 +60,52 @@
         <div>
             <table class="auto-style1">
                 <tr>
-                    <td>Id Producto:<asp:DropDownList class="auto-style3" ID="ddl_Producto" runat="server">
+                    <td class="auto-style14">
+                        <asp:Label ID="lblProducto" runat="server" Text="Id Producto:"></asp:Label>
+                    </td>
+                    <td class="auto-style18">
+                        <asp:DropDownList ID="ddl_Producto" runat="server">
                         <asp:ListItem Value="0">Igual a</asp:ListItem>
                         <asp:ListItem Value="1">Mayor a</asp:ListItem>
                         <asp:ListItem Value="2">Menor a</asp:ListItem>
                         </asp:DropDownList>
+                    </td>
+                    <td class="auto-style13">
                         <asp:TextBox class="auto-style3" ID="txtBox_Producto" runat="server" Width="280px"></asp:TextBox>
                     </td>
+                    <td class="auto-style11">
+                        <asp:RegularExpressionValidator ID="revIdProducto" runat="server" ControlToValidate="txtBox_Producto" ErrorMessage="Ingrese solo números" ValidationExpression="^\d+$" ValidationGroup="gpFiltrar" ForeColor="Red">*</asp:RegularExpressionValidator>
+                    </td>
                 </tr>
                 <tr>
-                    <td>Id Categoria:<asp:DropDownList style="margin-left: 16px;" ID="ddl_Categoria" runat="server">
+                    <td class="auto-style17">
+                        <asp:Label ID="lblCategoria" runat="server" Text="Id Categoria:"></asp:Label>
+                    </td>
+                    <td class="auto-style19">
+                        <asp:DropDownList ID="ddl_Categoria" runat="server">
                         <asp:ListItem Value="0">Igual a</asp:ListItem>
                         <asp:ListItem Value="1">Mayor a</asp:ListItem>
                         <asp:ListItem Value="2">Menor a</asp:ListItem>
                         </asp:DropDownList>
+                    </td>
+                    <td>
                         <asp:TextBox class="auto-style3" ID="txtBox_Categoria" runat="server" Width="280px"></asp:TextBox>
+                    </td>
+                    <td class="auto-style9">
+                        <asp:RegularExpressionValidator ID="revIdCategoria" runat="server" ControlToValidate="txtBox_Categoria" ErrorMessage="Ingrese solo números" ValidationExpression="^\d+$" ValidationGroup="gpFiltrar" ForeColor="Red">*</asp:RegularExpressionValidator>
                     </td>
                 </tr>
                 <tr>
-                    <td class="auto-style2">
-                        <div style="margin-left: 325px;">
-                        <asp:Button style="margin-right: 25px;" ID="btn_Filtrar" runat="server" Text="Filtrar" OnClick="btn_Filtrar_Click" />
-                        <asp:Button ID="btn_QuitarF" runat="server" Text="Quitar filtro" />
-                        </div>
+                    <td class="auto-style16">
                     </td>
+                    <td class="auto-style20">
+                        <asp:Button ID="btn_Filtrar" runat="server" Text="Filtrar" OnClick="btn_Filtrar_Click" ValidationGroup="gpFiltrar" />
+                    </td>
+                    <td class="auto-style5">
+                        <asp:Button ID="btn_QuitarF" runat="server" Text="Quitar filtro" OnClick="btn_QuitarF_Click" />
+                    </td>
+                    <td class="auto-style10">
+                        &nbsp;</td>
                 </tr>
             </table>
         </div>
--- TP4/Ejercicio2.aspx.cs
+++ TP4/Ejercicio2.aspx.cs
@@ -12,8 +12,32 @@ namespace TP4
 {
     public partial class Ejercicio2 : System.Web.UI.Page
     {
-                
-        string rutaDB = "Data Source = localhost\\SQLEXPRESS;Initial Catalog = Neptuno; Integrated Security = True";
+        private string rutaDB = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True;";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+
+            if (!IsPostBack)
+            {
+                DataSet ds = new DataSet();
+
+                // Se establece conexion con base de datos
+                SqlConnection cn = new SqlConnection(rutaDB);
+                cn.Open();
+
+                CargarDataSet(ds, cn,
+                    "SELECT IdProducto, NombreProducto, IdCategoría, CantidadPorUnidad, PrecioUnidad " +
+                    "FROM Productos",
+                    "Productos");
+
+                CargarGridView(ds);
+
+                // Se cierra la conexion
+                cn.Close();
+            }
+        }
+
         private void CargarGridView(DataSet ds)
         {
             gv_Productos.DataSource = ds.Tables["Productos"];
@@ -23,79 +47,115 @@ namespace TP4
         private void CargarDataSet(DataSet ds, SqlConnection cn, string comando, string tabla)
         {
             SqlDataAdapter adapt = new SqlDataAdapter();
-            
+
             adapt.SelectCommand = new SqlCommand(comando, cn);
             adapt.Fill(ds, tabla);
         }
 
-        private void FiltrarComparaciones(DataSet ds, SqlConnection cn, int idProducto, int signo)
+        private void FiltrarProductos(DataSet ds, SqlConnection cn, int idProducto, int signo)
         {
+            string filtro = "";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.Parameters.AddWithValue("@IdProducto", idProducto);
             switch (signo)
             {
                 case 0:
-                    CargarDataSetConParametros(ds, cn, idProducto, "SELECT IdProducto, NombreProducto, IdCategoría, CantidadPorUnidad, PrecioUnidad FROM Productos WHERE IdProducto = @IdProducto", "Productos");
+                    // Igual 
+                    filtro = "=";
                     break;
                 case 1:
+                    // Mayor
+                    filtro = ">";
                     break;
                 case 2:
+                    // Menor
+                    filtro = "<";
                     break;
             }
-
-
-        }
-        private void CargarDataSetConParametros(DataSet ds, SqlConnection cn, int idProducto, string comando, string tabla)
-        {
-            SqlCommand cmd = new SqlCommand(comando, cn);
-            cmd.Parameters.AddWithValue("@IdProducto", idProducto);
-
+            cmd.CommandText = "SELECT IdProducto, NombreProducto, IdCategoría, CantidadPorUnidad, PrecioUnidad " +
+                $"FROM Productos WHERE IdProducto {filtro} @IdProducto";
             SqlDataAdapter adapt = new SqlDataAdapter(cmd);
-
-            adapt.Fill(ds, tabla);
+            adapt.Fill(ds, "Productos");
         }
 
-
-        protected void Page_Load(object sender, EventArgs e)
+        private void FiltrarCategorias(DataSet ds, SqlConnection cn, int idCategoria, int signo)
         {
-            if (!IsPostBack)
+            string filtro = "";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.Parameters.AddWithValue("@IdCategoria", idCategoria);
+            switch (signo)
             {
-                DataSet ds = new DataSet();
-
-                // Se establece conexion con base de datos
-                SqlConnection cn = new SqlConnection(rutaDB);
-                cn.Open();
-
-                CargarDataSet(ds, cn,"SELECT IdProducto, NombreProducto, IdCategoría, CantidadPorUnidad, PrecioUnidad FROM Productos", "Productos");
-                    
-                CargarGridView(ds);
-
-                // Se cierra la conexion
-                cn.Close();
+                case 0:
+                    // Igual 
+                    filtro = "=";
+                    break;
+                case 1:
+                    // Mayor
+                    filtro = ">";
+                    break;
+                case 2:
+                    // Menor
+                    filtro = "<";
+                    break;
             }
+            cmd.CommandText = "SELECT IdProducto, NombreProducto, IdCategoría, CantidadPorUnidad, PrecioUnidad " +
+                $"FROM Productos WHERE IdCategoría {filtro} @IdCategoria " +
+                $"ORDER BY IdCategoría";
+            SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+            adapt.Fill(ds, "Productos");
         }
 
+        private void FiltrarXProductosYCategorias(DataSet ds, SqlConnection cn, int idProducto, int idCategoria, int tipoFiltroProducto, int tipoFiltroCategoria)
+        {
+
+        }
 
         protected void btn_Filtrar_Click(object sender, EventArgs e)
         {
             DataSet ds = new DataSet();
-
             SqlConnection cn = new SqlConnection(rutaDB);
 
             cn.Open();
 
-            if(!String.IsNullOrWhiteSpace(txtBox_Producto.Text) && String.IsNullOrWhiteSpace(txtBox_Categoria.Text)) {
-                int comparador = int.Parse(ddl_Producto.SelectedValue);
+            if (!String.IsNullOrWhiteSpace(txtBox_Producto.Text) && String.IsNullOrWhiteSpace(txtBox_Categoria.Text))
+            {
+                // Solo se filtra por producto
+                int tipoFiltro = int.Parse(ddl_Producto.SelectedValue);
                 int idProducto = int.Parse(txtBox_Producto.Text);
 
-                FiltrarComparaciones(ds, cn, idProducto, comparador);
+                FiltrarProductos(ds, cn, idProducto, tipoFiltro);
+            }
+            else if (String.IsNullOrWhiteSpace(txtBox_Producto.Text) && !String.IsNullOrWhiteSpace(txtBox_Categoria.Text))
+            {
+                // Solo se filtra por categoria
+                int tipoFiltro = int.Parse(ddl_Categoria.SelectedValue);
+                int idCategoria = int.Parse(txtBox_Categoria.Text);
 
-                CargarGridView(ds);
+                FiltrarCategorias(ds, cn, idCategoria, tipoFiltro);
+            }
+            else if (!String.IsNullOrWhiteSpace(txtBox_Producto.Text) && !String.IsNullOrWhiteSpace(txtBox_Categoria.Text))
+            {
+                // Se seleccionan ambos filtros
+                int tipoFiltroProducto = int.Parse(ddl_Producto.SelectedValue);
+                int idProducto = int.Parse(txtBox_Producto.Text);
+
+                int tipoFiltroCategoria = int.Parse(ddl_Categoria.SelectedValue);
+                int idCategoria = int.Parse(txtBox_Categoria.Text);
+                
+                // Completar funcion
+                FiltrarXProductosYCategorias(ds, cn, idProducto, idCategoria, tipoFiltroProducto, tipoFiltroCategoria);
+            }
+            else
+            {
+                // Cargar el gridview completo sin filtros
             }
 
             cn.Close();
 
+            CargarGridView(ds);
             LimpiarFiltros();
-            //txtBox_Producto.Text = "";
-            //txtBox_Categoria.Text = "";
         }
 
         private void LimpiarFiltros()
@@ -104,6 +164,11 @@ namespace TP4
             txtBox_Categoria.Text = "";
             ddl_Producto.SelectedIndex = 0;
             ddl_Categoria.SelectedIndex = 0;
+        }
+
+        protected void btn_QuitarF_Click(object sender, EventArgs e)
+        {
+            // Cargar el gridview completo sin filtros
         }
     }
 }
