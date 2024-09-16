13bf80a66e34d1d29abd914369aef6f5d87646ec
PuppyProgrammer <facuchena@hotmail.com>
2024-09-15 23:30:06 -0300

    modularizacion en ejercicio3

--- TP4/Ejercicio3.aspx.cs
+++ TP4/Ejercicio3.aspx.cs
@@ -6,29 +6,50 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
+using System.EnterpriseServices;
+using System.Data.SqlTypes;
 
 namespace TP4
 {
     public partial class Ejercicio3 : System.Web.UI.Page
     {
+        private SqlDataReader cargarDataReader(SqlConnection cn, string cmd)
+        {
+            SqlCommand consulta = new SqlCommand(cmd, cn);
+            return consulta.ExecuteReader();
+        }
+
+        private void cargarDdl(SqlDataReader dr, string item, string value)
+        {
+            ddlTemas.DataSource = dr;
+            ddlTemas.DataTextField = item;
+            ddlTemas.DataValueField = value;
+            ddlTemas.DataBind();
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
            string rutaLibreria = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Libreria;Integrated Security=True";
-          
-            if (!IsPostBack)
+            
+           if (!IsPostBack)
             {
                 SqlConnection cn = new SqlConnection(rutaLibreria);
                 cn.Open();
 
-               SqlCommand cmd = new SqlCommand ("SELECT * FROM Temas", cn);
 
-               SqlDataReader dr = cmd.ExecuteReader();
+                //SqlCommand cmd = new SqlCommand ("SELECT * FROM Temas", cn);
 
+                //SqlDataReader dr = cmd.ExecuteReader();
 
+                /*
                 ddlTemas.DataSource = dr;
                 ddlTemas.DataTextField = "Tema";
                 ddlTemas.DataValueField = "IdTema";
                 ddlTemas.DataBind();
+                */
+
+                SqlDataReader dr = cargarDataReader(cn, "SELECT * FROM Temas");
+
+                cargarDdl(dr, "Tema", "IdTema");
 
                 cn.Close();
                            
