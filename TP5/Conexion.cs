d333ec3999104bfd730ea9d2d800846f90d402fe
Ernesto José Riveiro <ernesto.ernzxr@gmail.com>
2024-09-23 12:21:01 -0300

    Se agregan las clases de Conexion y DML

---
+++ TP5/Conexion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TP5
+{
+    public class Conexion
+    {
+        private static string rutaConexion = "Data Source=localhost\\sqlexpress; Initial Catalog=BDSucursales; Integrated Security=True";
+        private SqlConnection cn = new SqlConnection(rutaConexion);
+
+        public DataTable ObtenerTablas(string consultaSQL, string nombreTabla)
+        {
+            cn.Open();
+            SqlDataAdapter adap = new SqlDataAdapter(consultaSQL, cn);
+            DataSet ds = new DataSet();
+            adap.Fill(ds, nombreTabla);
+            cn.Close();
+            return ds.Tables[nombreTabla];
+        }
+
+        public int EjecutarConsulta(string consultaSQL)
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand(consultaSQL, cn);
+            int filas = (int)cmd.ExecuteNonQuery(); // PARA INSERT-UPDATE-DELETE
+            cn.Close();
+            return filas;
+        }
+
+        public int EjecutarSP(SqlCommand Comando, String NombreSP)
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd = Comando;
+            cmd.Connection = cn;
+            cmd.CommandType = CommandType.StoredProcedure; // EJECUTA UN PROCEDIMIENTO ALMACENADO
+            cmd.CommandText = NombreSP;
+            int filas = cmd.ExecuteNonQuery(); // PARA INSERT-UPDATE-DELETE
+            cn.Close();
+            return filas;
+        }
+    }
+}
---
+++ TP5/DML.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace TP5
+{
+    public class DML
+    {
+        Conexion cn = new Conexion();
+
+        public DataTable ObtenerSucursales()
+        {
+            string consultaSQL = "SELECT Id_Sucursal AS ID, NombreSucursal AS NOMBRE, DescripcionProvincia AS PROVINCIA, DireccionSucursal AS DIRECCIÓN " +
+                "FROM Sucursal INNER JOIN Provincia ON Id_Provincia=Id_ProvinciaSucursal";
+            string nombreTabla = "Sucursales";
+            return cn.ObtenerTablas(consultaSQL, nombreTabla);
+        }
+
+        public DataTable ObtenerProvincias()
+        {
+            string consultaSQL = "SELECT * FROM Provincia";
+            string nombreTabla = "Provincias";
+            return cn.ObtenerTablas(consultaSQL, nombreTabla);
+        }
+
+        public DataTable ObtenerHorarios()
+        {
+            string consultaSQL = "SELECT * FROM Horario";
+            string nombreTabla = "Horarios";
+            return cn.ObtenerTablas(consultaSQL, nombreTabla);
+        }
+
+        public int AgregarSucursal(string nombre, string descripcion, string idProvincia, string direccion)
+        {
+            string consultaSQL = "INSERT INTO Sucursal (NombreSucursal, DescripcionSucursal, Id_ProvinciaSucursal, DireccionSucursal) " +
+                                 $"VALUES ('{nombre}', '{descripcion}', '{idProvincia}', '{direccion}')";
+            return cn.EjecutarConsulta(consultaSQL);
+        }
+
+        public int EliminarSucursal(string idSucursal)
+        {
+            string consultaSQL = "DELETE FROM Sucursal " +
+                $"WHERE Id_Sucursal={idSucursal}";
+            return cn.EjecutarConsulta(consultaSQL);
+        }
+
+        public DataTable BuscarSucursal(string idSucursal)
+        {
+            string consultaSQL = "SELECT Id_Sucursal AS ID, NombreSucursal AS NOMBRE, DescripcionProvincia AS PROVINCIA, DireccionSucursal AS DIRECCIÓN " +
+                "FROM Sucursal INNER JOIN Provincia ON Id_Provincia=Id_ProvinciaSucursal" +
+                $"WHERE Id+Sucursal={idSucursal}";
+            string nombreTabla = "Sucursales";
+            return cn.ObtenerTablas(consultaSQL, nombreTabla);
+        }
+    }
+}
