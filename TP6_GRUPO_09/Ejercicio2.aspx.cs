6fe83716820e4bb40164200961c1c0f6a9d69670
JustiLeon <juusleon@gmail.com>
2024-10-01 21:49:24 -0300

    Trabajo con formulario de inicio, cree webForm ej2

--- TP6_GRUPO_09/Ejercicio2.aspx.cs
+++ TP6_GRUPO_09/Ejercicio2.aspx.cs
@@ -7,12 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace TP6_GRUPO_09
 {
-    public partial class Inicio : System.Web.UI.Page
+    public partial class Ejercicio2 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            lblGrupo.Font.Bold = true;
-            lblGrupo.Font.Size = FontUnit.Point(20);
+
         }
     }
 }
