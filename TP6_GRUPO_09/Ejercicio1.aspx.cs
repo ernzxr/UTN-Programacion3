308e3ac60bc86c8e6bba85d5d5b2eac849925e62
Ernesto José Riveiro <ernesto.ernzxr@gmail.com>
2024-10-03 22:38:38 -0300

    Se carga el GridView y se le habilitan Edit, Delete y Pagination en Ejercicio1

--- TP6_GRUPO_09/Ejercicio1.aspx.cs
+++ TP6_GRUPO_09/Ejercicio1.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TP6_GRUPO_09.Utils;
 
 namespace TP6_GRUPO_09
 {
@@ -11,10 +12,21 @@ namespace TP6_GRUPO_09
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if(!IsPostBack)
+            {
+                CargarGridView();
+            }
         }
 
-        protected void grdProductos_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        private void CargarGridView()
+        {
+            Conexion con = new Conexion();
+            string querie = "SELECT IdProducto AS [Id Producto], NombreProducto AS [Nombre Producto], CantidadPorUnidad AS [Cantidad Por Unidad], PrecioUnidad AS [Precio Unidad] FROM Productos";
+            grdProductos.DataSource = con.ObtenerTablas(querie, "Productos");
+            grdProductos.DataBind();
+        }
+
+            protected void grdProductos_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
             grdProductos.PageIndex = e.NewPageIndex;
             //cargarGridView();
