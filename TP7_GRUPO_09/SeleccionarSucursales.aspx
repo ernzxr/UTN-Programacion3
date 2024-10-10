603adf51b23e1da2a1ff6c36e0543967770bcd18
PuppyProgrammer <facuchena@hotmail.com>
2024-10-09 23:31:46 -0300

    crea gestion ptovincia

--- TP7_GRUPO_09/SeleccionarSucursales.aspx
+++ TP7_GRUPO_09/SeleccionarSucursales.aspx
@@ -55,6 +55,9 @@
                 <tr>
                     <td class="auto-style4">
                         <asp:DataList ID="DataList1" runat="server">
+                            <ItemTemplate>
+                                <asp:Button ID="btn_Provincias" runat="server" />
+                            </ItemTemplate>
                         </asp:DataList>
                     </td>
                     <td colspan="3" style="padding-top:15px;" >
--- TP7_GRUPO_09/Utils/GestionProvincia.cs
+++ TP7_GRUPO_09/Utils/GestionProvincia.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,18 @@ namespace TP7_GRUPO_09.Utils
 {
     public class GestionProvincia
     {
+        private DataTable ObtenerTabla(String Nombre, String Sql)
+        {
+            DataSet ds = new DataSet();
+            Conexion datos = new Conexion();
+            SqlDataAdapter adp = datos.ObtenerAdaptador(Sql);
+            adp.Fill(ds, Nombre);
+            return ds.Tables[Nombre];
+        }
+
+        public DataTable ObtenerProvincias()
+        {
+            return ObtenerTabla("Provincias", "SELECT DescripcionProvincia FROM Provincias");
+        }
     }
 }
