eb3a1d8f911479c40173ebe2e291f35b58cf6c24
PuppyProgrammer <facuchena@hotmail.com>
2024-10-09 23:29:51 -0300

    Se crean las clases Provincia y GestionProvincia

---
+++ TP7_GRUPO_09/Utils/GestionProvincia.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP7_GRUPO_09.Utils
+{
+    public class GestionProvincia
+    {
+    }
+}
---
+++ TP7_GRUPO_09/Utils/Provincia.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP7_GRUPO_09.Utils
+{
+    public class Provincia
+    {
+        private int i_IdProvincia;
+        private string s_DescripicionProvincia;
+        public Provincia() {
+
+        }
+        public Provincia(int idProvincia, string DescripcionProvincia) {
+            this.i_IdProvincia = idProvincia;
+            this.s_DescripicionProvincia = DescripcionProvincia;
+        }
+
+        public int IdProvincia
+        {
+            get { return i_IdProvincia; }
+            set { i_IdProvincia = value; }
+        }
+
+        public string DescripicionProvincia
+        {
+            get { return s_DescripicionProvincia; }
+            set { s_DescripicionProvincia = value; }
+        }
+    }
+}
