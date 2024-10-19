59c0c901aa2c898c14297f2cbcc7fa20a9968605
VickyLuques <luquesvictoria559@gmail.com>
2024-10-19 13:59:27 -0300

    Se agregaron DaoProvincia (Dao), NegocioProvincia (Negocio), Provincia (Entidades) y la carga del ddlProvincias (ej1).

---
+++ TP8_GRUPO_09/Negocio/NegocioProvincia.cs
@@ -0,0 +1,20 @@
+﻿using Dao;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class NegocioProvincia
+    {
+        public DataTable getTabla()
+        {
+            DaoProvincia dao = new DaoProvincia();
+            return dao.getTablaProvincia();
+        }
+    }
+}
--- TP8_GRUPO_09/Vistas/AgregarSucursal.aspx.cs
+++ TP8_GRUPO_09/Vistas/AgregarSucursal.aspx.cs
@@ -4,14 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Negocio;
 
 namespace Vistas
 {
     public partial class AgregarSucursal : System.Web.UI.Page
     {
+        NegocioProvincia negProv = new NegocioProvincia();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                CargarProvincias();
+            }
+        }
 
+        private void CargarProvincias()
+        {
+            DataTable dt = negProv.getTabla();
+            ddlProvincias.DataSource = dt;
+            ddlProvincias.DataTextField = "DescripcionProvincia";
+            ddlProvincias.DataValueField = "Id_Provincia";
+            ddlProvincias.DataBind();
+
+            ddlProvincias.Items.Insert(0, new ListItem("-- Seleccionar --", "0"));
+            ddlProvincias.Items[0].Attributes["disabled"] = "disabled";
+            ddlProvincias.Items[0].Selected = true;
         }
     }
 }
