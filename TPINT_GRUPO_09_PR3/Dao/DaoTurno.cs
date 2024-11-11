5c1632b7444c376c9b617207892f1ca127e7f610
PuppyProgrammer <facuchena@hotmail.com>
2024-11-11 00:12:19 -0300

    Se agrega Entidad y DaoTurno

---
+++ TPINT_GRUPO_09_PR3/Dao/DaoTurno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao
+{
+    internal class DaoTurno
+    {
+        AccesoDatos ds = new AccesoDatos();
+
+        public DaoTurno()
+        {
+
+        }
+        public DataTable getTurnosMedico(string legajo)
+        {
+            string consulta = "SELECT * FROM Turnos WHERE Legajo_Medico_Tu = " + legajo;
+            return ds.ObtenerTabla("Turnos", consulta);
+        }
+
+    }
+}
---
+++ TPINT_GRUPO_09_PR3/Entidades/Turno.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Turno
+    {
+        private string _legajo_Medico;
+        private DateTime _fecha;
+        private TimeSpan _hora;
+        private string _DNI_Paciente;
+        private bool _asistencia;
+        private string _observacion;
+
+        public Turno()
+        {
+
+        }
+
+        public Turno(string legajo_Medico, DateTime fecha, TimeSpan hora, string Dni_Paciente, bool asistencia, string observacion)
+        {
+            this._legajo_Medico = legajo_Medico;
+            this._fecha = fecha;
+            this._hora = hora;
+            this._DNI_Paciente = Dni_Paciente;
+            this._asistencia = asistencia;
+            this._observacion = observacion;
+        }
+
+        public string getLegajo_Medico() {
+            return _legajo_Medico;
+        }
+        public DateTime getFecha() { 
+            return _fecha;
+        }
+        public TimeSpan getHora() {
+            return _hora;
+        }
+        public string getDNI_Paciente() {
+            return _DNI_Paciente;
+        }
+        public bool getAsistencia() { 
+            return _asistencia;
+        }
+        public string getObservacion()
+        {
+            return _observacion;
+        }
+        public void setLegajo_Medico(string legajo_Medico) {
+            this._legajo_Medico = legajo_Medico;
+        }
+
+        public void setFecha(DateTime fecha)
+        {
+            this._fecha = fecha;
+        }
+        public void setHora(TimeSpan hora)
+        {
+            this._hora = hora;
+        }
+        public void setAsistencia(bool asistencia)
+        {
+            this._asistencia = asistencia;
+        }
+        public void setObservacion(string observacion)
+        {
+            this._observacion = observacion;
+        }
+
+    }
+}
