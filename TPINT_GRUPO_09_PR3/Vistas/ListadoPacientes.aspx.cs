9c6f9c05bb630475e9564f12193a37789610638f
victoriaareyes <mavireyes2012@gmail.com>
2024-11-11 19:27:36 -0300

    Se agregó CargarNacionalidad en ListadoPacientes.

--- TPINT_GRUPO_09_PR3/Vistas/ListadoPacientes.aspx.cs
+++ TPINT_GRUPO_09_PR3/Vistas/ListadoPacientes.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Negocio;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,22 @@ namespace Vistas
 {
     public partial class ListadoPacientes : System.Web.UI.Page
     {
+        NegocioNacionalidad negn = new NegocioNacionalidad();
         protected void Page_Load(object sender, EventArgs e)
         {
             ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            CargarNacionalidad();
+        }
+
+        public void CargarNacionalidad()
+        {
+            DataTable Nacionalidad = negn.getTablaNacionalidad();
+            ddlNacionalidad.DataSource = Nacionalidad;
+            ddlNacionalidad.DataTextField = "Descripcion_Na";
+            ddlNacionalidad.DataValueField = "Id_Nacionalidad_Na";
+            ddlNacionalidad.DataBind();
+
+            ddlNacionalidad.Items.Insert(0, new ListItem("Seleccionar...", "0"));
         }
     }
 }
