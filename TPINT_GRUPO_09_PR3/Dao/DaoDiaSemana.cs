dedfa08c6bdcd1a667159e37af7f1784582d3b20
GabrielaLavia <gabriela.lavia@alumnos.frgp.edu.ar>
2024-11-11 16:22:24 -0300

    DaoDiaSemana y NegocioDiaSemana

---
+++ TPINT_GRUPO_09_PR3/Dao/DaoDiaSemana.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao
+{
+   public class DaoDiaSemana
+    {
+        AccesoDatos ds = new AccesoDatos();
+        public DaoDiaSemana() { }
+
+        public DataTable getDiaSemana()
+        {
+            string consulta = "SELECT * FROM Dias_Semanas";
+            return ds.ObtenerTabla("Dias_Semanas", consulta);
+        }
+
+    }
+}
---
+++ TPINT_GRUPO_09_PR3/Negocio/NegocioDiaSemana.cs
@@ -0,0 +1,21 @@
+﻿using Dao;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class NegocioDiaSemana
+    {
+        public NegocioDiaSemana() { }
+
+        public DataTable getTablaDiaSemana()
+        {
+            DaoDiaSemana dao = new DaoDiaSemana();
+            return dao.getDiaSemana();
+        }
+    }
+}
