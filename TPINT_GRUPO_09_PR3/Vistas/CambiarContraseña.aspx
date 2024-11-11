175fb0da752eca7d8ae676e41ac4f92f5c303337
JustiLeon <juusleon@gmail.com>
2024-11-11 19:19:29 -0300

    Se agregó CambiarContraseña.aspx

---
+++ TPINT_GRUPO_09_PR3/Vistas/CambiarContraseña.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="CambiarContraseña.aspx.cs" Inherits="Vistas.CambiarContraseña" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+    <title>Cambiar contraseña</title>
+    <style type="text/css">
+        .auto-style1 {
+            color: #006699;
+            font-weight: bold;
+            font-size: x-large;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+          .auto-style2 {
+        --bs-form-check-bg: var(--bs-body-bg);
+        flex-shrink: 0;
+        margin-top: .25em;
+        vertical-align: top;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        background-color: var(--bs-form-check-bg);
+        background-image: url('var(--bs-form-check-bg-image)');
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: contain;
+        -webkit-print-color-adjust: exact;
+        print-color-adjust: exact;
+    }
+</style>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <div style="width: 50%; display: flex; flex-direction: column; align-items: center; justify-content: center;">
+        <asp:Label ID="lblTitulo" runat="server" Text="Cambiar contraseña" CssClass="auto-style1"></asp:Label>
+
+        
+        <asp:Label ID="lblActual" runat="server" Text="Contraseña actual" Style="align-self: flex-start"></asp:Label>
+        <asp:TextBox ID="txtActual" TextMode="Password" CssClass="form-control" runat="server" ValidationGroup="grupo1"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="rfvActual" runat="server" ControlToValidate="txtActual" ForeColor="#CC0000" ValidationGroup="grupo1">(*) Complete el campo.</asp:RequiredFieldValidator>
+       <asp:RegularExpressionValidator ID="rfvPActual" runat="server" ControlToValidate="txtActual" CssClass="mb-3" ForeColor="#CC0000" ValidationExpression="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$" ValidationGroup="grupo1">(*) La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una minúscula y un número.</asp:RegularExpressionValidator>
+
+       <asp:Label ID="lblNueva" runat="server" Text="Nueva contraseña" Style="align-self: flex-start"></asp:Label>
+       <asp:TextBox ID="txtNueva" TextMode="Password" CssClass="form-control" runat="server" ValidationGroup="grupo1"></asp:TextBox>
+       <asp:RequiredFieldValidator ID="rfvNueva" runat="server" ControlToValidate="txtNueva" ForeColor="#CC0000" ValidationGroup="grupo1">(*) Complete el campo.</asp:RequiredFieldValidator>
+       <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ControlToValidate="txtNueva" CssClass="mb-3" ForeColor="#CC0000" ValidationExpression="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$" ValidationGroup="grupo1">(*) La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una minúscula y un número.</asp:RegularExpressionValidator>
+
+       <asp:Label ID="lblConfirme" runat="server" Text="Confirme la contraseña" Style="align-self: flex-start"></asp:Label>
+       <asp:TextBox ID="txtConfirme" TextMode="Password" CssClass="form-control" runat="server" ValidationGroup="grupo1"></asp:TextBox>
+       <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtConfirme" ForeColor="#CC0000" ValidationGroup="grupo1">(*) Complete el campo.</asp:RequiredFieldValidator>
+       <asp:RegularExpressionValidator ID="RegularExpressionValidator2" runat="server" ControlToValidate="txtConfirme" CssClass="mb-3" ForeColor="#CC0000" ValidationExpression="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$" ValidationGroup="grupo1">(*) La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una minúscula y un número.</asp:RegularExpressionValidator>      
+            
+        <asp:Button ID="btnConfirmar" class="btn btn-success" runat="server" Text="Confirmar" ValidationGroup="grupo1" OnClick="btnAgregar_Click" />
+        <asp:Label ID="lblMensaje" runat="server" Text=""></asp:Label>
+</div>
+        
+
+
+
+</asp:Content>
---
+++ TPINT_GRUPO_09_PR3/Vistas/CambiarContraseña.aspx.designer.cs
@@ -0,0 +1,152 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Vistas
+{
+
+
+    public partial class CambiarContraseña
+    {
+
+        /// <summary>
+        /// Control lblTitulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTitulo;
+
+        /// <summary>
+        /// Control lblActual.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblActual;
+
+        /// <summary>
+        /// Control txtActual.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtActual;
+
+        /// <summary>
+        /// Control rfvActual.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvActual;
+
+        /// <summary>
+        /// Control rfvPActual.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator rfvPActual;
+
+        /// <summary>
+        /// Control lblNueva.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNueva;
+
+        /// <summary>
+        /// Control txtNueva.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNueva;
+
+        /// <summary>
+        /// Control rfvNueva.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvNueva;
+
+        /// <summary>
+        /// Control RegularExpressionValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator1;
+
+        /// <summary>
+        /// Control lblConfirme.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblConfirme;
+
+        /// <summary>
+        /// Control txtConfirme.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtConfirme;
+
+        /// <summary>
+        /// Control RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
+        /// Control RegularExpressionValidator2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator2;
+
+        /// <summary>
+        /// Control btnConfirmar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnConfirmar;
+
+        /// <summary>
+        /// Control lblMensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensaje;
+    }
+}
