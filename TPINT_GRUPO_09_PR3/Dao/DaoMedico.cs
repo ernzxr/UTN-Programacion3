7b1b71bd6de711741cbec637c1cf43cb361a5586
GabrielaLavia <gabriela.lavia@alumnos.frgp.edu.ar>
2024-11-12 20:34:39 -0300

    Se completa Dao Medico

--- TPINT_GRUPO_09_PR3/Dao/DaoMedico.cs
+++ TPINT_GRUPO_09_PR3/Dao/DaoMedico.cs
@@ -18,6 +18,62 @@ namespace Dao
 
         }
 
+        public int agregarMedico(Medico medico)
+        {
+            SqlCommand cmd = new SqlCommand();
+            ArmarParametrosAgregarMedico(ref cmd, medico);
+            return ds.EjecutarProcedimientoAlmacenado(cmd, "spAgregarMedico");
+        }
+
+        private void ArmarParametrosAgregarMedico(ref SqlCommand cmd, Medico medico)
+        {
+            SqlParameter SqlParametros = new SqlParameter();
+
+            SqlParametros = cmd.Parameters.Add("@LEGAJO", SqlDbType.Char);
+            SqlParametros.Value = medico.getLegajo();
+
+            SqlParametros = cmd.Parameters.Add("@LOCALIDAD", SqlDbType.Int);
+            SqlParametros.Value = medico.getIdLocalidad();
+
+            SqlParametros = cmd.Parameters.Add("@ESPECIALIDAD", SqlDbType.Int);
+            SqlParametros.Value = medico.getIdEspecialidad();
+
+            SqlParametros = cmd.Parameters.Add("@NACIONALIDAD", SqlDbType.Int);
+            SqlParametros.Value = medico.getNacionalidad();
+
+            SqlParametros = cmd.Parameters.Add("@GENERO", SqlDbType.Int);
+            SqlParametros.Value = medico.getGenero();
+
+            SqlParametros = cmd.Parameters.Add("@USUARIO", SqlDbType.VarChar);
+            SqlParametros.Value = medico.getUsuario();
+
+            SqlParametros = cmd.Parameters.Add("@DNI", SqlDbType.Char);
+            SqlParametros.Value = medico.getDni();
+
+            SqlParametros = cmd.Parameters.Add("@EMAIL", SqlDbType.VarChar);
+            SqlParametros.Value = medico.getEmail();
+
+            SqlParametros = cmd.Parameters.Add("@NOMBRE", SqlDbType.VarChar);
+            SqlParametros.Value = medico.getEmail();
+
+            SqlParametros = cmd.Parameters.Add("@APELLIDO", SqlDbType.VarChar);
+            SqlParametros.Value = medico.getApellido();
+
+            SqlParametros = cmd.Parameters.Add("@FECHANACIMIENTO", SqlDbType.DateTime);
+            SqlParametros.Value = medico.getFechaNacimiento();
+
+            SqlParametros = cmd.Parameters.Add("@DIRECCION", SqlDbType.VarChar);
+            SqlParametros.Value = medico.getDireccion();
+
+            SqlParametros = cmd.Parameters.Add("@TELEFONO", SqlDbType.VarChar);
+            SqlParametros.Value = medico.getTelefono();
+
+            SqlParametros = cmd.Parameters.Add("@ESTADO", SqlDbType.Bit);
+            SqlParametros.Value = medico.getEstado();
+
+        }
+
+
         public DataTable getMedico()
         {
             string consulta = "SELECT * FROM Medicos";
