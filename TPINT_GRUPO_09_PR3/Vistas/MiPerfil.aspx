08859aef854454645b91e5cb4e312417288660d8
JustiLeon <juusleon@gmail.com>
2024-11-18 18:32:47 -0300

    Diseño form MiPerfil.aspx

---
+++ TPINT_GRUPO_09_PR3/Vistas/MiPerfil.aspx
@@ -0,0 +1,85 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="MiPerfil.aspx.cs" Inherits="Vistas.MiPerfil" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div style="display: flex; align-items: flex-start; justify-content: space-between; padding: 20px;">
+        <div class="container mt-5">
+        <h2>Perfil del Médico</h2>
+            <div class="text-center">
+            <!-- Imagen centrada -->
+            <img src="images/Card-6.jpg" alt="Imagen de perfil" class="mx-auto d-block" style="max-width: 200px; height: auto;" />
+        </div>
+            <asp:HyperLink ID="hlPass" runat="server" NavigateUrl="~/CambiarContraseña.aspx">Cambiar Clave</asp:HyperLink>
+            <table class="table table-bordered">
+                <!-- Título: Datos Médico -->
+                <tr>
+                    <th colspan="2" class="text-center">Datos Médico</th>
+                </tr>
+                <tr>
+                    <td><strong>Legajo:</strong></td>
+                    <td><asp:TextBox ID="txtLegajo" runat="server" CssClass="form-control"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><strong>Nombre:</strong></td>
+                    <td><asp:TextBox ID="txtNombreApellido" runat="server" CssClass="form-control"></asp:TextBox></td>
+                </tr>
+                 <tr>
+                    <td><strong>Apellido:</strong></td>
+                     <td><asp:TextBox ID="txtApellido" runat="server" CssClass="form-control"></asp:TextBox></td>
+                 </tr>
+                <tr>
+                    <td><strong>Especialidad</strong></td>
+                    <td><asp:TextBox ID="txtEspecialidad" runat="server" CssClass="form-control"></asp:TextBox></td>
+                 </tr>
+                <tr>
+                    <td><strong>Usuario:</strong></td>
+                    <td><asp:TextBox ID="txtUsuario" runat="server" CssClass="form-control"></asp:TextBox></td>
+                </tr>
+
+                <!-- Título: Datos Personales -->
+                <tr>
+                    <th colspan="2" class="text-center">Datos Personales</th>
+                </tr>
+                <tr>
+                     <td><strong>Dni</strong></td>
+                     <td><asp:TextBox ID="txtDni" runat="server" CssClass="form-control"></asp:TextBox></td>
+             </tr>
+                <tr>
+                    <td><strong>Fecha de Nacimiento:</strong></td>
+                    <td><asp:TextBox ID="txtNacimiento" runat="server" CssClass="form-control" TextMode="Date"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><strong>Dirección:</strong></td>
+                    <td><asp:TextBox ID="txtDireccion" runat="server" CssClass="form-control"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><strong>Provincia:</strong></td>
+                    <td><asp:TextBox ID="txtProvincia" runat="server" CssClass="form-control"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><strong>Nacionalidad:</strong></td>
+                    <td><asp:TextBox ID="txtNacionalidad" runat="server" CssClass="form-control"></asp:TextBox></td>
+                </tr>
+
+                <!-- Título: Contacto -->
+                <tr>
+                    <th colspan="2" class="text-center">Contacto</th>
+                </tr>
+                <tr>
+                    <td><strong>Email:</strong></td>
+                    <td><asp:TextBox ID="txtEmail" runat="server" CssClass="form-control" TextMode="Email"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><strong>Teléfono:</strong></td>
+                    <td><asp:TextBox ID="txtTelefono" runat="server" CssClass="form-control"></asp:TextBox></td>
+                </tr>
+            </table>
+
+           
+
+           
+    </div>
+
+</div>
+</asp:Content>
---
+++ TPINT_GRUPO_09_PR3/Vistas/MiPerfil.aspx.designer.cs
@@ -0,0 +1,134 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Vistas
+{
+
+
+    public partial class MiPerfil
+    {
+
+        /// <summary>
+        /// Control hlPass.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hlPass;
+
+        /// <summary>
+        /// Control txtLegajo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtLegajo;
+
+        /// <summary>
+        /// Control txtNombreApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreApellido;
+
+        /// <summary>
+        /// Control txtApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApellido;
+
+        /// <summary>
+        /// Control txtEspecialidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEspecialidad;
+
+        /// <summary>
+        /// Control txtUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsuario;
+
+        /// <summary>
+        /// Control txtDni.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDni;
+
+        /// <summary>
+        /// Control txtNacimiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNacimiento;
+
+        /// <summary>
+        /// Control txtDireccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDireccion;
+
+        /// <summary>
+        /// Control txtProvincia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtProvincia;
+
+        /// <summary>
+        /// Control txtNacionalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNacionalidad;
+
+        /// <summary>
+        /// Control txtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// Control txtTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTelefono;
+    }
+}
