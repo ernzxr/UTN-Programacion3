3032a5493b87629f140dfe17185221bac6f8524d
JustiLeon <juusleon@gmail.com>
2024-11-19 23:57:08 -0300

    Corrección navbar médicos

--- TPINT_GRUPO_09_PR3/Vistas/Main.Master.designer.cs
+++ TPINT_GRUPO_09_PR3/Vistas/Main.Master.designer.cs
@@ -195,15 +195,6 @@ namespace Vistas
         protected global::System.Web.UI.WebControls.HyperLink hlHistorial;
 
         /// <summary>
-        /// Control hlReportesInformesMedico.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink hlReportesInformesMedico;
-
-        /// <summary>
         /// Control hlPerfil.
         /// </summary>
         /// <remarks>
