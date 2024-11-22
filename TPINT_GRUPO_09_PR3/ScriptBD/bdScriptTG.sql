1d31692fc57f50f048df9f92096d1a36d7b7c248
Ernesto José Riveiro <ernesto.ernzxr@gmail.com>
2024-11-22 18:24:51 -0300

    Se agrega TRIGGER para pasar los turnos a pendientes cuando un medico se da de baja

--- TPINT_GRUPO_09_PR3/ScriptBD/bdScriptTG.sql
+++ TPINT_GRUPO_09_PR3/ScriptBD/bdScriptTG.sql
@@ -1,2 +1,17 @@
 USE UTN2C2024PR3CLINICA
 GO
+
+CREATE OR ALTER TRIGGER tgBajaMedicoBajaTurnos
+ON Medicos
+AFTER UPDATE
+AS
+BEGIN
+    UPDATE Turnos
+    SET Id_Ciclo_Turno_Tu = 2,
+        Id_Detalle_Turno_Tu = 2,
+		Estado_Tu = 0
+    FROM Turnos Tu
+    INNER JOIN Inserted I ON Tu.Legajo_Medico_Tu = I.Legajo_Me
+    INNER JOIN Deleted D ON I.Legajo_Me = D.Legajo_Me
+    WHERE I.Estado_Me = 0 AND D.Estado_Me <> 0 AND Tu.Estado_Tu = 1 AND Tu.Id_Ciclo_Turno_Tu = 1
+END
