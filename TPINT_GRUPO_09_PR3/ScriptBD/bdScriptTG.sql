94d20366186ad459d23b251ab740aaf3cf2b5789
Ernesto José Riveiro <ernesto.ernzxr@gmail.com>
2024-11-22 18:38:27 -0300

    Se agrega TRIGGER para pasar los turnos a pendientes cuando se carga una ausencia medica y coincide con un turno activo

--- TPINT_GRUPO_09_PR3/ScriptBD/bdScriptTG.sql
+++ TPINT_GRUPO_09_PR3/ScriptBD/bdScriptTG.sql
@@ -15,3 +15,20 @@ BEGIN
     INNER JOIN Deleted D ON I.Legajo_Me = D.Legajo_Me
     WHERE I.Estado_Me = 0 AND D.Estado_Me <> 0 AND Tu.Estado_Tu = 1 AND Tu.Id_Ciclo_Turno_Tu = 1
 END
+GO
+
+CREATE OR ALTER TRIGGER tgAusenciaMedicoBajaTurnos
+ON Ausencias_Medicos
+AFTER INSERT
+AS
+BEGIN
+    UPDATE Turnos
+    SET Estado_Tu = 0,
+        Id_Ciclo_Turno_Tu = 2,
+        Id_Detalle_Turno_Tu = 2
+    FROM Turnos Tu
+    INNER JOIN Inserted I ON Tu.Legajo_Medico_Tu = I.Legajo_Medico_AM
+    WHERE Tu.Fecha_Tu BETWEEN I.Fecha_Inicio_AM AND I.Fecha_Fin_AM 
+		AND Tu.Estado_Tu = 1 AND Tu.Id_Ciclo_Turno_Tu = 1
+END
+GO
