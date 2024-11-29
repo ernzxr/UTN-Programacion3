aeaa03fcd4a76bb93d55f56ae32fb3f3bf4e17cc
victoriaareyes <mavireyes2012@gmail.com>
2024-11-29 00:22:33 -0300

    Se agrega tgBajaPaciente

--- TPINT_GRUPO_09_PR3/ScriptBD/bdScriptTG.sql
+++ TPINT_GRUPO_09_PR3/ScriptBD/bdScriptTG.sql
@@ -32,3 +32,19 @@ BEGIN
 		AND Tu.Estado_Tu = 1 AND Tu.Id_Ciclo_Turno_Tu = 1
 END
 GO
+
+CREATE OR ALTER TRIGGER tgBajaPaciente
+ON Pacientes
+AFTER UPDATE
+AS
+BEGIN
+    UPDATE Turnos
+    SET Id_Ciclo_Turno_Tu = 5,
+        Id_Detalle_Turno_Tu = 4,
+		Estado_Tu = 0
+    FROM Turnos Tu
+    INNER JOIN Inserted I ON Tu.DNI_Paciente_Tu = I.DNI_Pa AND Tu.Id_Nacionalidad_Paciente_Tu = I.Id_Nacionalidad_Pa
+    INNER JOIN Deleted D ON I.DNI_Pa = D.DNI_Pa AND I.Id_Nacionalidad_Pa = D.Id_Nacionalidad_Pa
+    WHERE I.Estado_Pa = 0 AND D.Estado_Pa <> 0
+END
+GO
